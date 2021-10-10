a6347803db9c27a6a42206dabc1a054ffe2f6bc4
xitren <xitren@ya.ru>
2021-10-10 23:02:32 +0300

    -

--- Matlab/home_correction.m
+++ Matlab/home_correction.m
@@ -6,7 +6,7 @@ gf = 1:size(ECG,1);
 gy = 1:size(PPG,1);
 
 subplot(4,1,1); 
-[up_ecg, lo_ecg] = interval_detector2(ECG, 600);
+[up_ecg, lo_ecg] = interval_detector2(ECG, 400);
 peaks_ecg = find(ECG == up_ecg');
 [peaks_cg, ex_cg ] = interval_corrector(peaks_ecg, 1000, 7);
 plot(gf, ECG(gf), 'g-', gf, PPG(gf), 'b-',...
@@ -16,7 +16,7 @@ axis([1000 inf -1000 1000])
 
 subplot(4,1,3); 
 [grad_ppg] = gradient(PPG);
-[up_ppg, lo_ppg] = interval_detector2(grad_ppg, 600);
+[up_ppg, lo_ppg] = interval_detector2(grad_ppg, 400);
 plot(1:size(grad_ppg), grad_ppg, 1:size(up_ppg,2), up_ppg, ...
     1:size(lo_ppg,2), lo_ppg);
 axis([1000 inf -200 200])
@@ -29,8 +29,8 @@ delta_peak = [0;(peaks_g(2:end) - peaks_g(1:end-1))];
 delta_ex = [0;(ex_g(2:end) - ex_g(1:end-1))];
 delta_peak = delta_peak - mean(delta_peak);
 delta_ex = delta_ex - mean(delta_ex);
-peaks_g = int32(peaks_g);
-ex_g = int32(ex_g);
+% peaks_g = int32(peaks_g);
+% ex_g = int32(ex_g);
 plot(gf, PPG(gf), 'g-', gf, ECG(gf), 'b-',...
      peaks_g, PPG(peaks_g), 'rx ');
 % plot(gy, PPG(gy), 'g-',...
@@ -38,9 +38,29 @@ plot(gf, PPG(gf), 'g-', gf, ECG(gf), 'b-',...
 title('RANSAC on data');
 axis([0 inf -1000 1000])
 
+peaks_tg = zeros(0,1);
+for i = 1:(size(peaks_g,1))
+    x1 = peaks_g(i) - 5;
+    x2 = peaks_g(i) + 5;
+    y1 = PPG(x1);
+    y2 = PPG(x2);
+    y = min(PPG( (peaks_g(i)-200):(peaks_g(i)+200) ));
+    x = ((y - y1) * (x2 - x1) / (y2 - y1)) + x1;
+    peaks_tg = [peaks_tg ; x];
+end
+
 subplot(4,1,3);
-plot(peaks_g, delta_peak, 'b- ', ex_g, delta_ex, 'r- ');
-title('Epoch');
+PAT2 = zeros(0,1);
+for i = 1:size(peaks_cg,1)
+    ind = find( (peaks_cg(i) < peaks_tg) & (peaks_tg < (peaks_cg(i) + 600)) );
+    if (size(ind) > 0)
+        PAT2 = [PAT2 ; [peaks_cg(i), peaks_tg(ind(1)) - peaks_cg(i)]];
+    end
+end
+plot(PAT2(:,1), PAT2(:,2), 'b- ');
+% plot(PAT2(:,1), PAT2(:,2), 'b- ', timePat, PAT-50, 'b- ');
+% axis([0 inf 100 200]);
+title('PAT');
 
 coff_ecg = lo_ecg' - up_ecg';
 data_source_norm_ecg = (ECG - (lo_ecg')) ./ coff_ecg;
@@ -49,3 +69,11 @@ data_source_norm_ecg = abs(data_source_norm_ecg);
 coff_ppg = lo_ppg' - up_ppg';
 data_source_norm_ppg = (PPG - (lo_ppg')) ./ coff_ppg;
 data_source_norm_ppg = abs(data_source_norm_ppg);
+
+subplot(4,1,4);
+vq1 = interp1(PAT2(:,1), PAT2(:,2), gf);
+vq2 = tsmovavg(vq1,'s',100000);
+vq2 = (vq2 - 150) / 150;
+plot(gf, vq2, 'b- ');
+axis([0 inf 0 1]);
+save('corrected.mat', 'ECG','PPG','vq2');
--- Matlab/home_epoch_tg.m
+++ Matlab/home_epoch_tg.m
@@ -1,40 +1,26 @@
-home_correction;
+clear;
+load('corrected.mat');
 
-e_var = -100;
+all_size = size(vq2,2)/2;
 e_size = 700;
-epochs_ecg = zeros(e_size, 0);
-epochs_ppg = zeros(e_size, 0);
-pts = zeros(3, 0);
-k = e_var;
-for i = 3:size(peaks_cg)-2
-%     for k = -300:20:300
-        z1 = peaks_cg(i) + k;
-        z2 = peaks_cg(i) + k + e_size - 1;
-        fff = find((z1 < peaks_g) & (peaks_g < z2));
-        if size(fff,1) > 0
-            pts = [pts , [peaks_cg(i)-z1 ; peaks_g(fff(1))-z1 ; 0]];
-            ep = ECG(z1 : z2);
-            min_ep = min(ep);
-            ep = ep - min_ep;
-            max_ep = max(ep);
-            ep = ep / max_ep;
-            epochs_ecg = [epochs_ecg , ep];
-            ep = PPG(z1 : z2);
-            min_ep = min(ep);
-            ep = ep - min_ep;
-            max_ep = max(ep);
-            ep = ep / max_ep;
-            epochs_ppg = [epochs_ppg , ep];
-        end
-%     end
+epoch_ecg = zeros(all_size - 700,e_size);
+epoch_ppg = zeros(all_size - 700,e_size);
+epoch_result = zeros(all_size - 700,1);
+for i=(701:all_size)
+    rr = ECG( (i - e_size):(i - 1) );
+    min_rr = min(rr);
+    max_rr = max(rr);
+    rr = (rr - min_rr) ./ (max_rr - min_rr);
+    rr2 = PPG( (i - e_size):(i - 1) );
+    min_rr2 = min(rr2);
+    max_rr2 = max(rr2);
+    rr2 = (rr2 - min_rr2) ./ (max_rr2 - min_rr2);
+    epoch_ecg(i-700,:) = rr;
+    epoch_ppg(i-700,:) = rr2;
+    if (isnan(vq2(i-700)))
+        epoch_result(i-700,1) = 0;
+    else
+        epoch_result(i-700,1) = vq2(i-700);
+    end
 end
-
-for i = 1:size(epochs_ppg,2)
-    x1 = pts(2,i) - 5;
-    x2 = pts(2,i) + 5;
-    y1 = epochs_ppg(x1,i);
-    y2 = epochs_ppg(x2,i);
-    y = min(epochs_ppg(:,i));
-    x = ((y - y1) * (x2 - x1) / (y2 - y1)) + x1;
-    pts(3,i) = x;
-end
+% save('epochs_all.mat', 'epoch_ecg','epoch_ppg','vq2', '-v7.3');
